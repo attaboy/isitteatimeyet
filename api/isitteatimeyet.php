701ecf1c0fbc4ebee761925aa17103bd37e54bcb
Ryan Greenberg <greenberg@twitter.com>
2011-06-10 15:59:36 -0700

    Add support for XML

--- api/isitteatimeyet.php
+++ api/isitteatimeyet.php
@@ -8,6 +8,17 @@ switch($format) {
   case 'json':
     echo json_encode(array('teatime' => $teatime));
     break;
+  case 'xml':
+    header('Content-type: text/xml');
+    $xml_teatime = $teatime ? 'true' : 'false';
+    $xml = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<teatime>
+  <$xml_teatime />
+</teatime>
+XML;
+    echo $xml;
+    break;
   default:
     header("HTTP/1.0 406 Not Acceptable");
     echo "Invalid format";
